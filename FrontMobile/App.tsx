bc45d4584fe0024a9af8f91fed280a8cae67c94d
Jhon <jhon@jhon.Ienovo>
2022-09-17 21:48:38 -0300

    Alterações e novas implementações de código

--- FrontMobile/App.tsx
+++ FrontMobile/App.tsx
@@ -1,43 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import{
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}> Testando React Native!</Text>
-
-      <Button title='Send 1'/>
-      <Button title='Send 2'/>
-      <Button title='Send 3'/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-interface ButtonProps{
-  title: string
-}
-
-function Button(props: ButtonProps){
+export default function App(){
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
   return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home/> : <Loading/>}
+    </Background>
   )
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#000',
-    fontSize: 26
-  }
-});
